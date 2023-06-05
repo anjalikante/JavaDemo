527469bc7120edc2b4e8a2b8857f31e27ed9b4cf
anjalikante <anjali.kante@gmail.com>
2023-06-05 12:38:59 +0530

    sysout changed

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,6 @@ public class Main {
         System.out.println("For remote june latest");
     }
     public void test(){
-        System.out.println("For test");
+        System.out.println("changes done");
     }
 }
